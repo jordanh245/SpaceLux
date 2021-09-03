f8855df7e6c6b7f5c20d5f60b5d8eb3f7c405b89
Jordan Harris <Jordanharris245@gmail.com>
2021-09-03 13:44:37 -0400

    Update page4.js

--- page4.js
+++ page4.js
@@ -1,10 +1,10 @@
 async function fetchNewsData() {
 	const data = await fetch(
-		// "https://gnews.io/api/v4/search?q=nasa&country=us&language=en&token=5c72d223e8cefeb8e540657a176433a9"
+		"https://gnews.io/api/v4/search?q=nasa&country=us&language=en&token=5c72d223e8cefeb8e540657a176433a9"
 	
 
 		// News API doesnt work on github pages
-		  "https://newsapi.org/v2/everything?q=nasa&from=&sortBy=publishedAt/language=en/&apiKey=6acaa9b934d54b039a523d604ed51b38"
+		//   "https://newsapi.org/v2/everything?q=nasa&from=&sortBy=publishedAt/language=en/&apiKey=6acaa9b934d54b039a523d604ed51b38"
 	);
 	const newsData = await data.json();
 	const newsArticles = document.querySelector(".newsArt");
@@ -20,10 +20,10 @@ async function fetchNewsData() {
 	 
 	  header.innerText = article.title;
 	  textBody.innerText = article.description;
-	//   image.src = article.image
-	  image.src = article.urlToImage
-		? article.urlToImage
-		//  ? article.image
+	  image.src = article.image
+	//   image.src = article.urlToImage
+		// ? article.urlToImage
+		 ? article.image
 		: "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png";
 	  
 		
